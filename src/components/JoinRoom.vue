73ecb55f11f6464486050db4c5b2db28b544e447
Kovács Áron László <k.aron.laszlo@gmail.com>
2018-08-09 19:59:04 +0200

    [modify]: add content, srcipts, and un petit style (in ChatRoom.vue) to components

--- src/components/JoinRoom.vue
+++ src/components/JoinRoom.vue
@@ -1,11 +1,61 @@
 <template>
 
+  <b-row>
+
+    <b-col col="6">
+
+      <h2>
+        Join
+        <b-link href="#/">(Room List)</b-link>
+      </h2>
+      <b-form @submit="onSubmit">
+
+        <b-form-group id="fieldsetHorizontal"
+                      horizontal
+                      :label-cols="4"
+                      breakpoint="md"
+                      label="Enter Nickname">
+          <b-form-input id="nickname" :state="state" v-model.trim="chat.nickname"></b-form-input>
+        </b-form-group>
+        <b-button type="submit" variant="primary"></b-button>
+
+      </b-form>
+
+    </b-col>
+
+  </b-row>
+
 </template>
 
 <script>
-    export default {
-        name: "JoinRoom"
-    }
+
+import axios from 'axios'
+
+export default {
+  name: "JoinRoom",
+  data() {
+      return {
+        chat: {}
+      }
+  },
+  methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        this.chat.room = this.$route.params.id
+        this.chat.massega = this.chat.nickname + ' join the room'
+        axios.post(`http://localhost:300/api/chat`, this.chat)
+          .then(response => {
+            this.$router.push({
+              name: 'ChatRoom',
+              params: {id: this.$route.params.id, nickname: response.data.nickname}
+            })
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+      }
+  }
+}
 </script>
 
 <style scoped>
