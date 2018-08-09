fb8bf91142afa7f3b000d1085e84334e9c2b14ef
Kovács Áron László <k.aron.laszlo@gmail.com>
2018-08-09 19:04:05 +0200

    [create]: + create vue project components + call components in index.js + install bootstrap-vue + call bootstrap-vue in main.js + install axios

---
+++ src/components/AddRoom.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "AddRoom"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/JoinRoom.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "JoinRoom"
+    }
+</script>
+
+<style scoped>
+
+</style>
